ffbd78a28bf1c1ba6f41301ff923d7f59f43eedb
Dhakal-Silas <silas.dhakal@gmail.com>
2022-07-01 23:12:36 +0545

    complete with animation

--- app.js
+++ app.js
@@ -8,4 +8,62 @@ const mobileMenu = () => {
     menuLinks.classList.toggle('active')
     body.classList.toggle('active')
 }
-menu.addEventListener('click',mobileMenu)
+menu.addEventListener('click',mobileMenu);
+
+//Animations
+gsap.registerPlugin(ScrollTrigger);
+gsap.from('.animate-hero',{
+    duration:0.6,
+    opacity:0,
+    y: -150,
+    stagger:0.3
+});
+gsap.from('.animate-services',{
+    scrollTrigger:'.animate-services',
+    duration:0.5,
+    opacity:1,
+    x: -150,
+    stagger:0.12
+});
+
+gsap.from('.animate-img',{
+    scrollTrigger:'.animate-services',
+    duration:1.2,
+    opacity:0,
+    x: -200,
+    
+});
+
+gsap.from('.animate-membership',{
+    scrollTrigger:'.animate-membership',
+    duration:1,
+    opacity:0,
+    y: -150,
+    stagger:0.3,
+    delay:0.5
+});
+gsap.from('.animate-card',{
+    scrollTrigger:'.animate-card',
+    duration:1,
+    opacity:0,
+    y: -150,
+    stagger:0.2,
+    delay:0.5
+});
+
+gsap.from('.animate-team',{
+    scrollTrigger:'.animate-team',
+    duration:1,
+    opacity:0,
+    y: -150,
+    stagger:0.3,
+    delay:0.2
+});
+gsap.from('.animate-email',{
+    scrollTrigger:'.animate-email',
+    duration:0.8,
+    opacity:1,
+    y: -150,
+    stagger:0.25,
+    delay:0.4
+});
